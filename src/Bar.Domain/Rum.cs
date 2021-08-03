3d3ca2cd85bcbe45e850ee69f763af1adeb656d1
Olaf Kober <olaf.kober@outlook.coom>
2021-08-03 13:20:43 +0200

    added entity for Rum

---
+++ src/Bar.Domain/Rum.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2021, Olaf Kober <olaf.kober@outlook.com>
+
+using System;
+using System.Collections.Generic;
+
+
+namespace Bar.Domain
+{
+    /// <summary>
+    ///     Represents a Rum.
+    /// </summary>
+    public sealed class Rum
+    {
+        /// <summary>
+        ///     The unique Id of the Rum.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        ///     The name of the Rum, e.g. "Clément Rhum Blanc".
+        /// </summary>
+        public String? Name { get; set; }
+
+        /// <summary>
+        ///     A teaser of the Rum, e.g. "Martinique".
+        /// </summary>
+        public String? Teaser { get; set; }
+
+        /// <summary>
+        ///     A list of image file names of the Rum, e.g. "KRO01084.jpg", "KRO00410.jpg".
+        /// </summary>
+        public IList<String> Images { get; set; } = new List<String>();
+    }
+}
