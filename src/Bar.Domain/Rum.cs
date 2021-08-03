967b7ad124f43aa55627e44cb3d7f6f3242f0045
Olaf Kober <olaf.kober@outlook.coom>
2021-08-03 17:08:27 +0200

    added domain objects

---
+++ src/Bar.Domain/Rum.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) 2021, Olaf Kober <olaf.kober@outlook.com>
+
+using System;
+using System.Collections.Generic;
+
+
+namespace Bar.Domain
+{
+    /// <summary>
+    ///     Represents a Rum.
+    /// </summary>
+    /// 
+    /// <param name="Id">
+    ///     The unique Id of the Rum.
+    /// </param>
+    /// <param name="Name">
+    ///     The name of the Rum, e.g. "Clément Rhum Blanc".
+    /// </param>
+    public sealed record Rum(Guid Id, String Name)
+    {
+        /// <summary>
+        ///     A teaser of the Rum, e.g. "Martinique".
+        /// </summary>
+        public String Teaser { get; init; } = "";
+
+        /// <summary>
+        ///     A list of images of the Rum.
+        /// </summary>
+        public IReadOnlyList<Image> Images { get; init; } = Array.Empty<Image>();
+    }
+}
