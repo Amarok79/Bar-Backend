aa1eb6b9d5b3fa668e871caf4bb481bda6c41e0a
Olaf Kober <olaf.kober@outlook.coom>
2021-08-03 14:16:13 +0200

    added first simple controller

---
+++ src/Bar.Backend/Controllers/RumController.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2021, Olaf Kober <olaf.kober@outlook.com>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bar.Data;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Bar.Backend.Controllers
+{
+    [ApiController, Route("api/rums")]
+    public sealed class RumController : ControllerBase
+    {
+        private readonly BarDbContext mDbContext;
+
+
+        public RumController(BarDbContext dbContext)
+        {
+            mDbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IList<String>> GetAll()
+        {
+            IQueryable<String>? dto = mDbContext.Rums.Select(x => x.Name);
+
+            return Ok(dto);
+        }
+    }
+}
