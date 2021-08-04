e0cf3b067fa57c3dfe193c7543d68275abf90db9
Olaf Kober <olaf.kober@outlook.coom>
2021-08-04 12:37:40 +0200

    introduced mapping extensions

---
+++ src/Bar.Data/RumExtensions.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) 2021, Olaf Kober <olaf.kober@outlook.com>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bar.Domain;
+
+
+namespace Bar.Data
+{
+    public static class RumExtensions
+    {
+        public static RumDbo ToDbo(this Rum entity)
+        {
+            return new RumDbo {
+                Id     = entity.Id,
+                Name   = entity.Name,
+                Teaser = entity.Teaser,
+                Images = String.Join(';', entity.Images.Select(x => x.FileName)),
+            };
+        }
+
+        public static Rum ToEntity(this RumDbo dbo)
+        {
+            return new Rum(dbo.Id, dbo.Name) {
+                Teaser = dbo.Teaser ?? String.Empty,
+                Images = mapImages(),
+            };
+
+
+            IReadOnlyList<Image> mapImages()
+            {
+                return dbo.Images == null
+                    ? Array.Empty<Image>()
+                    : dbo.Images.Split(';', StringSplitOptions.RemoveEmptyEntries).Select(x => new Image(x)).ToList();
+            }
+        }
+    }
+}
