4dd1e3006904e4b7cd001f5d23e4c660cca9a66c
Olaf Kober <olaf.kober@outlook.coom>
2021-08-09 18:54:30 +0200

    added Gin to domain

---
+++ tests/Bar.Domain.Tests/Domain/GinTests.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) 2021, Olaf Kober <olaf.kober@outlook.com>
+
+using System;
+using NFluent;
+using NUnit.Framework;
+
+
+namespace Bar.Domain
+{
+    [TestFixture]
+    public class GinTests
+    {
+        [Test]
+        public void Usage()
+        {
+            var id = Guid.NewGuid();
+
+            var Gin = new Gin(id, "The Stin Dry Gin");
+
+            Check.That(Gin.Id).IsEqualTo(id);
+            Check.That(Gin.Name).IsEqualTo("The Stin Dry Gin");
+            Check.That(Gin.Teaser).IsEmpty();
+            Check.That(Gin.Images).IsEmpty();
+
+            Gin = new Gin(id, "The Stin Dry Gin") {
+                Teaser = "Styrian Dry Gin",
+                Images = new[] { new Image("KRO01046.jpg"), new Image("KRO00364.jpg") },
+            };
+
+            Check.That(Gin.Id).IsEqualTo(id);
+            Check.That(Gin.Name).IsEqualTo("The Stin Dry Gin");
+            Check.That(Gin.Teaser).IsEqualTo("Styrian Dry Gin");
+            Check.That(Gin.Images).HasSize(2);
+            Check.That(Gin.Images[0].FileName).IsEqualTo("KRO01046.jpg");
+            Check.That(Gin.Images[1].FileName).IsEqualTo("KRO00364.jpg");
+
+            Gin = Gin with { Teaser = "foo" };
+
+            Check.That(Gin.Id).IsEqualTo(id);
+            Check.That(Gin.Name).IsEqualTo("The Stin Dry Gin");
+            Check.That(Gin.Teaser).IsEqualTo("foo");
+            Check.That(Gin.Images).HasSize(2);
+            Check.That(Gin.Images[0].FileName).IsEqualTo("KRO01046.jpg");
+            Check.That(Gin.Images[1].FileName).IsEqualTo("KRO00364.jpg");
+        }
+    }
+}
