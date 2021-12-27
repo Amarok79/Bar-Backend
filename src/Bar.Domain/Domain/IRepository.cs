89a6ac37c812071feada349a70db82beb12572c7
Olaf Kober <olaf.kober@outlook.com>
2021-12-27 20:35:00 +0100

    introduced base interface

---
+++ src/Bar.Domain/Domain/IRepository.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) 2021, Olaf Kober <olaf.kober@outlook.com>
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Bar.Domain;
+
+/// <summary>
+///     Represents a repository of <typeparamref name="TItem"/>.
+/// </summary>
+public interface IRepository<in TId, TItem>
+{
+    /// <summary>
+    ///     Gets all items.
+    /// </summary>
+    Task<IReadOnlyList<TItem>> GetAllAsync();
+
+    /// <summary>
+    ///     Gets the item with the given Id, or null if not existing.
+    /// </summary>
+    /// 
+    /// <param name="id">
+    ///     The Id of the item to return.
+    /// </param>
+    Task<TItem?> GetOrDefaultAsync(TId id);
+
+    /// <summary>
+    ///     Deletes the item with the given Id.
+    /// </summary>
+    /// 
+    /// <param name="id">
+    ///     The Id of the item to delete.
+    /// </param>
+    /// <returns>
+    ///     True, if the item has been deleted; False, if not existing.
+    /// </returns>
+    Task<Boolean> DeleteAsync(TId id);
+
+    /// <summary>
+    ///     Adds a new item or updates an existing one.
+    /// </summary>
+    /// 
+    /// <param name="item">
+    ///     The item to add or update.
+    /// </param>
+    /// <returns>
+    ///     True, if a new item has been added, otherwise False if an existing item has been updated.
+    /// </returns>
+    Task<Boolean> AddOrUpdateAsync(TItem item);
+}
